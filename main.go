6fc1b05a279cd23be6697cdabc0ce483a119eddc
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2025-02-16 09:36:46 +0530

    Fyne POC

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+	"os"
+)
+
+func main() {
+	os.Setenv("FYNE_FONT", `C:\Users\vpaij\AppData\Local\Microsoft\Windows\Fonts\RecMonoCasualNerdFont-Regular.ttf`)
+	os.Setenv("FYNE_FONT", `C:\Windows\Fonts\comic.ttf`)
+	a := app.New()
+	w := a.NewWindow("Hello")
+
+	hello := widget.NewLabel("Hello Fyne!")
+	w.SetContent(container.NewVBox(
+		hello,
+		widget.NewButton("Hi!", func() {
+			hello.SetText("huh")
+		}),
+	))
+
+	w.ShowAndRun()
+}
