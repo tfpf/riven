a977b893fc3740e7eb318422c71b4866bce0b812
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2025-02-16 11:30:12 +0530

    Formatted

--- config/config.go
+++ config/config.go
@@ -1,25 +1,25 @@
 package config
 
 import (
-"os"
-"path/filepath"
+	"os"
+	"path/filepath"
 )
 
-type Config struct{
-    FyneFont string `json:"FYNE_FONT"`
+type Config struct {
+	FyneFont string `json:"FYNE_FONT"`
 }
 
 // NewConfig looks for the Riven configuration JSON file in the OS-specific
 // user configuration directory and loads it.
-func NewConfig() (*Config, error){
-    configDir, err := os.UserConfigDir()
-    if err != nil{
-        return nil, err
-    }
-    configFile := filepath.Join(configDir, "riven", "config.json")
-    _, err = os.Open(configFile)
-    if err != nil{
-        return nil, err
-    }
-    return nil, nil
+func NewConfig() (*Config, error) {
+	configDir, err := os.UserConfigDir()
+	if err != nil {
+		return nil, err
+	}
+	configFile := filepath.Join(configDir, "riven", "config.json")
+	_, err = os.Open(configFile)
+	if err != nil {
+		return nil, err
+	}
+	return nil, nil
 }
