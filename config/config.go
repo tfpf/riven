fe5d2b77ef0d03019eeebcda3bb75d0de1cf9483
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2025-02-23 18:51:40 +0530

    Reset to e6adafa

--- config/config.go
+++ config/config.go
@@ -8,7 +8,7 @@ import (
 )
 
 type Config struct {
-	FyneFont string `json:"fyneFont,omitempty"`
+	FyneFont string `json:"FYNE_FONT,omitempty"`
 }
 
 // Read looks for the Riven configuration JSON file in the OS-specific user
